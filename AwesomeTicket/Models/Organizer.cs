6b6cecd4327cbf7a311da2f76209f770551e74b3
Yuanyuan Zhou <W0504770@campus.nscc.ca>
2025-09-25 12:13:17 -0300

    Create Organizer.cs

---
+++ AwesomeTicket/Models/Organizer.cs
@@ -0,0 +1,11 @@
+﻿namespace AwesomeTicket.Models
+{
+    public class Organizer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ContactEmail { get; set; }
+        public string PhoneNumber { get; set; }
+        public List<Show> Shows { get; set; }
+    }
+}
