1f207c7b59f0d4f249eb9d983341517652279807
Yuanyuan Zhou <W0504770@campus.nscc.ca>
2025-09-25 12:03:23 -0300

    Add project files.

---
+++ AwesomeTicket/Controllers/ShowsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AwesomeTicket.Models;
+
+namespace AwesomeTicket.Controllers
+{
+    public class ShowsController : Controller
+    {
+        // 模拟数据（替代数据库）
+        private static List<Show> shows = new List<Show>
+        {
+            new Show { Id = 1, Title = "Concert", Location = "Halifax", Date = DateTime.Now.AddDays(7) },
+            new Show { Id = 2, Title = "Tech Meetup", Location = "NSCC", Date = DateTime.Now.AddDays(14) }
+        };
+
+        public IActionResult Index()
+        {
+            return View(shows);
+        }
+    }
+}
---
+++ AwesomeTicket/Models/Show.cs
@@ -0,0 +1,11 @@
+﻿namespace AwesomeTicket.Models
+{
+    public class Show
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string Location { get; set; }
+
+    }
+}
